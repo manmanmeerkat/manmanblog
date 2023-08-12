edc0608dbc63e51413b9c1b959f787fd3dd931f4
manmanmeer <masak6161@gmail.com>
2023-08-12 14:39:14 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { client } from "../libs/client";
+import styles from "../styles/Home.module.scss";
+
+
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint:"blog" });
+  
+
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
+
+export default function Home({ blog }) {
+  return (
+    
+    <div className={styles.container}>
+      {blog.map((blog) => (
+        <li key={blog.id}>
+          <Link href={`blog/${blog.id}`}>
+            {blog.title}
+          </Link>
+        </li>
+      ))}
+      </div>
+  )
+}
